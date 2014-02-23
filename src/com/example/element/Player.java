e37356a0f2aa750cafa536d9f1555ef6c6b14fd6
AoziversaliuS <1220310838@qq.com>
2014-02-23 16:47:46 +0800

    Revert "封印"
    This reverts commit 2de70ee4620c974965ea4b847c6c9450613dc70c.

--- src/com/example/element/Player.java
+++ src/com/example/element/Player.java
@@ -19,6 +19,7 @@ public class Player extends OzElement{
 	
 	
 	public  static final float VALUE_JUMP    = 7;  //跳跃的速度
+	
 	public  static final int JumpTimeMAX = 50;
 	private static int JumpTimeCount = 0;  //跳跃的时间
 	
@@ -26,8 +27,8 @@ public class Player extends OzElement{
 	public static PlaneE     planeT    =  PlaneE.ELSE;
 	//verticalTouch
 	public static VerticalE  verticalT =  VerticalE.ELSE;
-	private static  boolean jump = false;
-	//此
+	private static boolean jump = false;
+	
 	public Player() {
 		super(
 				"Player",
